c39528f8d7a24ebabefa84c321a4a881a410ccef
Hazama Fawkes <56687898+Hazama01@users.noreply.github.com>
2019-10-25 01:24:11 +0800

    lavender: COSP-fy

--- cosp_lavender.mk
+++ cosp_lavender.mk
@@ -14,11 +14,10 @@ $(call inherit-product, device/xiaomi/lavender/device.mk)
 
 # Inherit some common BootLeggers stuff.
 TARGET_BOOT_ANIMATION_RES := 1080
-BOOTLEGGERS_BUILD_TYPE := Shishufied
-$(call inherit-product, vendor/bootleggers/config/common_full_phone.mk)
+$(call inherit-product, vendor/cosp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := bootleg_lavender
+PRODUCT_NAME := cosp_lavender
 PRODUCT_DEVICE := lavender
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7
